b288940e4ba102f55ad45f16f4e962645fb052a5
DK\dmsa <dmsa@sitecore.net>
2018-08-30 18:01:19 +0300

    Original code from Sitecore 9.0 Update-2

---
+++ src/Sitecore.Support.233798/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.233798")]
+[assembly: AssemblyProduct("Sitecore.Support.233798")]
+[assembly: ComVisible(false)]
